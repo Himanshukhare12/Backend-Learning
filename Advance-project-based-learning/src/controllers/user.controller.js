77ff7eb83177c5df699f7de4764c22e62882d512
Himanshu khare <hkhare666@gmail.com>
2026-01-15 22:21:11 +0530

    add auth middleware and logout controller

--- Advance-project-based-learning/src/controllers/user.controller.js
+++ Advance-project-based-learning/src/controllers/user.controller.js
@@ -107,4 +107,28 @@ const loginUser = asyncHandler(async (req,res) => {
     )
 })
 
-export { registerUser}
+const logoutUser = asyncHandler(async (req, res) => {
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set: {
+                refreshToken: undefined
+            }
+        },
+        {
+            new: true
+        }
+    )
+    const options = {
+        httpOnly : true,
+        secure : true
+    }
+    res.status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(
+        new ApiResponse(200, {}, "User logged out successfully")
+    )
+})
+
+export { registerUser, loginUser, logoutUser };
