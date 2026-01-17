b683e973803359a120a80506965e672eba57f03b
Himanshu khare <hkhare666@gmail.com>
2026-01-17 19:33:18 +0530

    Add refresh token functionality and update user routes

--- Advance-project-based-learning/src/controllers/user.controller.js
+++ Advance-project-based-learning/src/controllers/user.controller.js
@@ -131,4 +131,30 @@ const logoutUser = asyncHandler(async (req, res) => {
     )
 })
 
-export { registerUser, loginUser, logoutUser };
+const refreshAccessToken = asyncHandler(async (req, res)=>{
+    const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken;
+    if(!incomingRefreshToken)
+    throw new ApiError(401, "Unauthorized request")
+    try {
+        const decodedToken = jwt.verify(incomingRefreshToken, process.env.REFRESH_TOKEN_SECRET)
+        const user= await User.findById(decodedToken?._id)
+        if(!user)
+        throw new ApiError(404, "Invalid refresh token")
+        if(user?.refreshToken !== incomingRefreshToken)
+        throw new ApiError(401, "RefreshToken is expired or used")
+        const options = {
+            httpOnly : true,
+            secure : true
+        }
+        const {accessToken, newRefreshToken} = await generateAccessAndRefreshTokens(user._id)
+        return res.status(200).cookie("refreshToken", newRefreshToken, options)
+        .cookie("accessToken", accessToken, options)
+        .json(
+            new ApiResponse(200, {accessToken, refreshToken: newRefreshToken}, "Access token refreshed successfully")
+        )
+    } catch (error) {
+        throw new ApiError(401, error?.message || "Invalid refresh token")
+    }
+})
+
+export { registerUser, loginUser, logoutUser, refreshAccessToken };
